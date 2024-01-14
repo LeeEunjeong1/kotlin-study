6d088962d9023100daf0ffcf3b3904842a73493e
eunjeong Lee <tbig1019@gmail.com>
2024-01-14 14:03:50 +0900

    level1

---
+++ src/main/kotlin/programmers/level1/콜라츠 추측.kt
@@ -0,0 +1,15 @@
+package programmers.level1
+
+fun solution(num: Int): Int {
+    return collatz(num.toLong(),0)
+}
+fun collatz(num:Long,answer:Int):Int{
+    if(answer+1 == 500) return -1
+    if(answer == 0 && num == 1L) return 0
+    if(num == 1L) return answer
+    return if(num%2 == 0L){
+        collatz(num/2, answer+1 )
+    }else{
+        collatz(num*3+1, answer+1 )
+    }
+}
